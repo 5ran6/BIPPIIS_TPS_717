3605e7859c7f66db8b85b24cbe4862ecb095baff
5ran6 <5raan6@gmail.com>
2019-12-25 13:04:13 +0100

    Initial commit

---
+++ app/src/test/java/wrappers/greenbit/com/dactymatch_gui_javaandroid/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package wrappers.greenbit.com.dactymatch_gui_javaandroid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
